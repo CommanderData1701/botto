70b6dc4428eb1369f727cec8b8c708b9dd689caa
Linus Henke <linus.henke@mci.edu>
2024-09-24 10:59:21 +0200

    Started testing.

--- botto/message_handlers.py
+++ botto/message_handlers.py
@@ -6,13 +6,13 @@ class Done(Enum):
 
 
 class Handler:
-    def __init__(self):
+    def __init__(self) -> None:
         raise NotImplementedError
 
     def generate_response(self, message: str) -> str:
         raise NotImplementedError
 
-    def __call__(self):
+    def __call__(self) -> dict[str, str]:
         raise NotImplementedError
 
 
@@ -22,21 +22,26 @@ class SetupHandler(Handler):
         CONFIRM_NAME = lambda name: f"Hello, {name}! Is this correct? (yes/no)"
         CHANGE_NAME = "Ok, what is it then?"
         SET_UP_USERS = f"Great! Now tell us who your roommates are. (Seperated by commas)"
-        CONFIRM_USERS = lambda users: f"""Are these the correct roommates? 
-        {users} (yes/no)"""
+        CONFIRM_USERS = lambda users: f"Are {users} your roommates? (yes/no)"
         CHANGE_USERS = "Ok, who are they then?"
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.state = self.State.BEGIN
+
+    def __call__(self) -> dict[str, str]:
+        if self.state != Done.DONE:
+            raise ValueError("Handler is not done yet")
+
+        return {"root_name": self.root_name, "roommates": self.users}
 
     def generate_response(self, message: str) -> str:
         match self.state:
             case self.State.BEGIN:
                 self.state = self.State.CONFIRM_NAME
-                self.root_name = message
                 return self.State.BEGIN.value
 
             case self.State.CONFIRM_NAME:
+                self.root_name = message
                 self.state = self.State.CHANGE_NAME
                 return self.State.CONFIRM_NAME(message)
 
@@ -51,5 +56,19 @@ class SetupHandler(Handler):
                     return "Answer must be 'yes' or 'no'"
 
             case self.State.SET_UP_USERS:
-                self.state = next(self.state)
-                return self.State.SET_UP_USERS.value
+                self.users = message.split(",")
+                self.users = [user.strip() for user in self.users]
+                self.state = self.State.CONFIRM_USERS
+                user_string = ", ".join(self.users)
+                return self.State.CONFIRM_USERS(user_string)
+
+            case self.State.CONFIRM_USERS:
+                self.state = Done.DONE
+                if message.lower() == "yes":
+                    return Done.DONE.value
+                elif message.lower() == "no":
+                    self.state = self.State.CHANGE_USERS
+                    return self.State.CHANGE_USERS
+                else:
+                    return "Answer must be 'yes' or 'no"
+
---
+++ botto/tests/test_message_handlers.py
@@ -0,0 +1,45 @@
+import unittest
+
+from ..message_handlers import (
+    Done,
+    SetupHandler
+)
+
+
+class TestInitialSetupHandler(unittest.TestCase):
+
+    def test_initial_setup_without_name_change(self):
+        handler = SetupHandler()
+        self.assertEqual(handler.state, SetupHandler.State.BEGIN)
+
+        response = handler.generate_response("Hello")
+
+        self.assertEqual(response, "Hello! You are now the root user. What's your name?")
+        self.assertEqual(handler.state, SetupHandler.State.CONFIRM_NAME)
+
+        response = handler.generate_response("John")
+
+        self.assertEqual(response, "Hello, John! Is this correct? (yes/no)")
+        self.assertEqual(handler.state, SetupHandler.State.CHANGE_NAME)
+
+        response = handler.generate_response("yes")
+        self.assertEqual(response, "Great! Now tell us who your roommates are. (Seperated by commas)")
+        self.assertEqual(handler.state, SetupHandler.State.SET_UP_USERS)
+
+        response = handler.generate_response("John, Jane, Doe")
+        expected_response = """Are John, Jane, Doe your roommates? (yes/no)"""
+        self.assertEqual(response, expected_response)
+        self.assertEqual(handler.state, SetupHandler.State.CONFIRM_USERS)
+
+        response = handler.generate_response("yes")
+        self.assertEqual(response, "All set!")
+        self.assertEqual(response, "All set!")
+        result_dict = {"root_name": "John", "roommates": ["John", "Jane", "Doe"]}
+        self.assertEqual(handler(), result_dict)
+
+    def test_initial_setup_with_name_change(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
